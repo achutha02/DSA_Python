e2d2f5f2d8c8634cfca7e86f77d438af8e93db26
achutha02 <bidarahalliachyuta@gmail.com>
2026-02-05 17:05:21 +0530

    Updated file

--- patterns/pattern9.py
+++ patterns/pattern9.py
@@ -26,4 +26,28 @@ for i in range(n):
 
 
 # Another way
+def erected_pyramid(n):
+    for i in range(n):
+        for j in range(n-i-1):
+            print(" ", end='')
+        
+        for k in range(2*i+1):
+            print("*", end='')
+        
+        print()
 
+def inverted_pyramid(n):
+    for i in range(n):
+        for j in range(i):
+            print(" ", end='')
+        
+        for k in range(2*n-(2*i+1)):
+            print("*", end='')
+        
+        print()
+
+def pattern9(n):
+    erected_pyramid(n)
+    inverted_pyramid(n)
+
+pattern9(5)
