1bb143062635269fbf2ad17d212569bfc52c1ba8
achutha02 <bidarahalliachyuta@gmail.com>
2026-02-23 08:42:29 +0530

    Added new file

---
+++ Basic_strings/sort_characters_by_frequency.py
@@ -0,0 +1,13 @@
+s = "banana"
+
+freq = [(0, chr(i + ord('a'))) for i in range(26)]
+
+for ch in s:
+    index = ord(ch) - ord('a')
+    freq[index] = (freq[index][0] + 1, ch)
+
+freq.sort(key= lambda x : (-x[0], x[1]))
+
+result = [ch for f, ch in freq if f > 0]
+
+print(result)
