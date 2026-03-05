87c25a38d7f22fed9eee5255a5282fb0f2cdc3ec
achutha02 <bidarahalliachyuta@gmail.com>
2026-03-05 11:27:52 +0530

    Added new file

---
+++ patterns/pattern10(another_approach).py
@@ -0,0 +1,13 @@
+import sys
+
+
+sys.stdin = open('input.txt','r')
+sys.stdout = open('output.txt', 'w')
+
+n = int(input())
+for i in range(1,2*n):
+    stars = i if i <=n else 2*n-i
+    for j in range(1,stars+1):
+        print("*",end='')
+    
+    print()
