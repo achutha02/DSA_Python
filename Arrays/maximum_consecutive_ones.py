97f5a59dc34091dbc914bc0bec87c20beea0ca56
achutha02 <bidarahalliachyuta@gmail.com>
2026-03-27 16:48:32 +0530

    added new file

---
+++ Arrays/maximum_consecutive_ones.py
@@ -0,0 +1,17 @@
+nums = [1, 1, 0, 0, 1, 1, 1, 0]
+
+count = 0
+largest_count = 0
+
+for i in range(len(nums)):
+    if nums[i] == 1:
+        count += 1
+
+        largest_count = max(largest_count, count) # Or using if condition
+    
+    else:
+        count = 0
+
+print(largest_count)
+
+    
